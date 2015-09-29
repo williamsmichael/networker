95a6686c7d71beba3fd0907cac7fc8b0b89b7c17
williamsmichael <willia32@hotmail.com>
2015-09-29 11:33:22 -0700

    creates tables for group, group-user, message system

--- networker/group/models.py
+++ networker/group/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import datetime
+from user.models import User
 
-# Create your models here.
+class Group(models.Model):
+	""" Main table for Group """
+	group_organizer = models.ForeignKey(User)
+	group_name = models.CharField(max_length=255)
+	group_description = models.TextField(max_length=255, blank=True)
+	created_dateTime = models.DateTimeField(auto_now_add=True)
+
+class Group_User(models.Model):
+	""" Relationship table for message system and user """
+	group_id = models.ForeignKey(Group)
+	user_id = models.ForeignKey(User)
+	last_message_dateTime = models.DateTimeField(default=datetime.now())
+
+class Message_System_Topic(models.Model):
+	""" Topic table for message system """
+	group_id = models.ForeignKey(Group)
+	originator_id = models.ForeignKey(Group_User)
+	topic_name = models.TextField()
+	topic_description = models.TextField()
+	created_dateTime = models.DateTimeField(auto_now_add=True)
+
+class Message_System_Message(models.Model):
+	message_system_topic_id = models.ForeignKey(Message_System_Topic)
+	group_user_id = models.ForeignKey(Group_User)
+	message = models.TextField()
+	created_dateTime = models.DateTimeField(auto_now_add=True)
+
+
+
