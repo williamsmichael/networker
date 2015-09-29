af3673bbe91fefb0c9ce3a24d5b628c50d5d9b24
williamsmichael <willia32@hotmail.com>
2015-09-29 11:32:40 -0700

    edits triple quotes text

--- networker/user/models.py
+++ networker/user/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from datetime import datetime
 from django.utils import timezone
 
-# Create your models here.
 class User(models.Model):
-    """ This is the main table for User """
+    """ Main table for User """
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -19,21 +18,21 @@ class User(models.Model):
     is_admin = models.BooleanField(default=False)
 
 class Skill_Category(models.Model):
-	""" This is a helper table for User_Skill """
+	""" Helper table for User_Skill """
     skill_category = models.CharField(max_length=20)
 
 class User_Skill(models.Model):
-	""" This is a sub-table for the User: Skill """
+	""" Sub-table for the User: Skill """
 	user_id = models.ForeignKey(User)
 	skill_category_id = models.ForeignKey(Skill_Category)
 	skill_description = models.TextField(max_length=255, blank=True)
 
 class Address_Category(models.Model):
-	""" This is a helper table for User_Address """
+	""" Helper table for User_Address """
     address_category = models.CharField(max_length=10)
 
 class User_Address(models.Model):
-	""" This is a sub-table for the User: Address """
+	""" Sub-table for the User: Address """
 	user_id = models.ForeignKey(User)
 	address_category_id = models.ForeignKey(Address_Category)
 	street_address_1 = models.CharField(max_length=50)
@@ -46,42 +45,42 @@ class User_Address(models.Model):
 	longitude_api = models.CharField(max_length=25)
 
 class Email_Category(models.Model):
-	""" This is a helper table for User_Email """
+	""" Helper table for User_Email """
     email_category = models.CharField(max_length=10)
 
 class User_Email(models.Model):
-	""" This is a sub-table for the User: Email """
+	""" Sub-table for the User: Email """
 	user_id = models.ForeignKey(User)
 	email_category_id = models.ForeignKey(Address_Category)
 	email = models.CharField(max_length=50, blank=True)
 
 class Phone_Category(models.Model):
-	""" This is a helper table for User_Phone """
+	""" Helper table for User_Phone """
     phone_category = models.CharField(max_length=10)
 
 class User_Phone(models.Model):
-	""" This is a sub-table for the User: Phone """
+	""" Sub-table for the User: Phone """
 	user_id = models.ForeignKey(User)
 	phone_category_id = models.ForeignKey(Phone_Category)
 	country_code = models.PositiveSmallIntegerField(max_length=3, blank=True)
 	phone_number = models.PositiveSmallIntegerField(max_length=15, blank=True)
 
 class Social_Media_Category(models.Model):
-	""" This is a helper table for User_Social_Media """
+	""" Helper table for User_Social_Media """
 	social_media_category = models.CharField(max_length=50)
 
 class User_Social_Media(models.Model):
-	""" This is a sub-table for the User: Social Media """
+	""" Sub-table for the User: Social Media """
 	user_id = models.ForeignKey(User)
 	social_media_category_id = models.ForeignKey(Social_Media_Category)
 	social_media_url = models.URLField(blank=True)
 
 class Job_Category(models.Model):
-	""" This is a helper table for User_Job """
+	""" Helper table for User_Job """
 	job_category = models.CharField(max_length=50)
 
 class User_Job(models.Model):
-	""" This is a sub-table for the User: Job """
+	""" Sub-table for the User: Job """
 	user_id = models.ForeignKey(User)
 	job_category_id = models.ForeignKey(Job_Category)
 	job_description = models.TextField(max_length=255, blank=True)
@@ -92,11 +91,11 @@ class User_Job(models.Model):
 	company_year_ended = models.DateField(blank=True)
 
 class Education_Category(models.Model):
-	""" This is a helper table for User_Education """
+	""" Helper table for User_Education """
 	education_category = models.CharField(max_length=50)
 
 class User_Education(models.Model):
-	""" This is a sub-table for the User: Education """
+	""" Sub-table for the User: Education """
 	user_id = models.ForeignKey(User)
 	education_category_id = models.ForeignKey(Education_Category)
 	education_description = models.TextField(max_length=255, blank=True)
