154dddc1cfad649cf6d074bdbe292d290e6f526f
williamsmichael <willia32@hotmail.com>
2015-09-28 18:16:34 -0700

    adds email + phone user tables

--- networker/user/models.py
+++ networker/user/models.py
@@ -10,14 +10,16 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     relationship_to_group = models.CharField(max_length=50)
     # profile_image = ImageField()
-    email = models.CharField(max_length=50)
+    primary_email = models.CharField(max_length=50, unique=True)
     date_of_birth = models.DateTimeField()
     created_dateTime = models.DateTimeField(auto_now_add=True)
     last_login_dateTime = models.DateTimeField(default=datetime.now())
     user_timezone = timezone.now()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
-class Skill_Category(model.Model):
-	""" This is a helper table for User_Skills """
+class Skill_Category(models.Model):
+		""" This is a helper table for User_Skill """
 		SKILL_CATEGORY_CHOICES = (
 				(ADMINISTRATIVE, 'Administrative'),
 				(CLERICAL, 'Clerical'),
@@ -35,14 +37,50 @@ class Skill_Category(model.Model):
         (TRAINING, 'Training'),
         (SALES, 'Sales'),
     )
-    skill_category = models.CharField(max_length=25,
+    skill_category = models.CharField(max_length=50,
                                       choices=SKILL_CATEGORY_CHOICES)
 
-class User_Skill(model.Model):
-	""" This is a sub-table for User storing Skills """
+class User_Skill(models.Model):
+		""" This is a sub-table for the User: Skill """
 		user_id = models.ForeignKey(User)
 		skill_category_id = models.ForeignKey(Skill_Category)
 		skill_description = models.TextField(max_length=255)
+
+class Email_Category(models.Model):
+		""" This is a helper table for User_Email """
+		EMAIL_CATEGORY_CHOICES = (
+				(HOME, 'Home'),
+				(WORK, 'Work'),
+				(OTHER, 'Other'),
+    )
+    email_category = models.CharField(max_length=50,
+                                      choices=EMAIL_CATEGORY_CHOICES)
+
+class User_Email(models.Model):
+		""" This is a sub-table for the User: Email """
+		user_id = models.ForeignKey(User)
+		email_category_id = models.ForeignKey(Email_Category)
+		email = models.CharField(max_length=50)
+
+class Phone_Category(models.Model):
+		""" This is a helper table for User_Phone """
+		PHONE_CATEGORY_CHOICES = (
+				(Mobile, 'Mobile'),
+				(WORK, 'Work'),
+				(HOME, 'Home'),
+				(FAX, 'Fax'),
+				(OTHER, 'Other'),
+    )
+    phone_category = models.CharField(max_length=50,
+                                      choices=PHONE_CATEGORY_CHOICES)
+
+class User_Phone(models.Model):
+		""" This is a sub-table for the User: Phone """
+		user_id = models.ForeignKey(User)
+		phone_category_id = models.ForeignKey(Email_Category)
+		country_code = models.PositiveSmallIntegerField(max_length=3)
+		phone_number = models.PositiveSmallIntegerField(max_length=15)
+
 
 
 
