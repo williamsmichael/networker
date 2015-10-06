f477a64ab27b8453a6cbf8bfaace35a4f08f9fc6
williamsmichael <willia32@hotmail.com>
2015-10-06 10:27:49 -0700

    creates base html and home page

--- networker/networker/urls.py
+++ networker/networker/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    # new url patterns should be coded above
+    url(r'^$', views.home),
 ]
