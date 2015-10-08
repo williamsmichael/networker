a623e6c064332aea23c220ca348c1374ac094874
williamsmichael <willia32@hotmail.com>
2015-10-08 01:27:36 -0700

    adds messageSystemTopicTag to admin

--- networker/group/admin.py
+++ networker/group/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import NetworkerGroup, GroupUser, MessageSystemTopic, MessageSystemMessage
+from .models import NetworkerGroup, GroupUser, MessageSystemTopic, MessageSystemTopicTag, MessageSystemMessage
 
 # Register your models here.
 admin.site.register(NetworkerGroup)
 admin.site.register(GroupUser)
 admin.site.register(MessageSystemTopic)
+admin.site.register(MessageSystemTopicTag)
 admin.site.register(MessageSystemMessage)
 
